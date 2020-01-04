cb3dce75d6ea040f58ed50b4ab09fc2896effcb3
HazemEmam <Hazem.emam20150198@yahoo.com>
2020-01-04 11:50:25 +0200

    Initial commit

---
+++ InfoWare/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InfoWare.Startup))]
+namespace InfoWare
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
